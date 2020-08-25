a5abe89093d202394368d60156592540e6722568
Mateusz Marczuk <marczuk@live.it>
2020-08-25 13:05:17 +0000

    Add state publisher class

---
+++ src/Shared/common/src/common/state_publisher.cpp
@@ -0,0 +1,38 @@
+/**
+ *Copyright ( c ) 2020, KNR Selfie
+ *This code is licensed under BSD license (see LICENSE for details)
+ **/
+
+#include <common/state_publisher.h>
+#include <string>
+
+StatePublisher::StatePublisher(const std::string& topic_name, float pub_freq) : nh_("~")
+{
+    publisher_ = nh_.advertise<std_msgs::Int8>(topic_name, 1);
+    state_.data = 0;  // UNINITIALIZED
+    timer_ = nh_.createTimer(ros::Duration(pub_freq), &StatePublisher::timerCallback, this);
+    timer_.start();
+    ROS_INFO("State publisher timer started on %s topic", topic_name.c_str());
+}
+
+void StatePublisher::updateState(int8_t new_state)
+{
+    state_.data = new_state;
+    sendState();
+}
+
+void StatePublisher::updateState(std_msgs::Int8 new_state)
+{
+    state_ = new_state;
+    sendState();
+}
+
+void StatePublisher::timerCallback(const ros::TimerEvent& event)
+{
+    sendState();
+}
+
+void StatePublisher::sendState()
+{
+    publisher_.publish(state_);
+}
