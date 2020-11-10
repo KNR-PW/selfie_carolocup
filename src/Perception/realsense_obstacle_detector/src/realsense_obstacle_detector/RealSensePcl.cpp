5d46984cade0c493018ce13ea137f2756d1541f7
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2020-11-10 20:02:34 +0100

    WIP:Implemented primitive plane filtering

--- src/Perception/realsense_obstacle_detector/src/realsense_obstacle_detector/RealSensePcl.cpp
+++ src/Perception/realsense_obstacle_detector/src/realsense_obstacle_detector/RealSensePcl.cpp
@@ -5,11 +5,14 @@ void RealSensePcl::pc2Callback(const sensor_msgs::PointCloud2 &msg)
   pcl::PointCloud<pcl::PointXYZRGB> cloud;
   pcl::fromROSMsg(msg, cloud);
 
-  pcl::PointCloud<pcl::PointXYZHSV> cloud_hsv;
-  convertPCXYZRGBtoXYZHSV(cloud, cloud_hsv);
+  // pcl::PointCloud<pcl::PointXYZHSV> cloud_hsv;
+  // convertPCXYZRGBtoXYZHSV(cloud, cloud_hsv);
 
-  pcl::PointCloud<pcl::PointXYZHSV> cloud_filtered;
-  filterColorFromPC(cloud_hsv, cloud_filtered);
+  // pcl::PointCloud<pcl::PointXYZHSV> cloud_filtered;
+  // filterColorFromPC(cloud_hsv, cloud_filtered);
+
+  pcl::PointCloud<pcl::PointXYZRGB> cloud_filtered;
+  filterFloorFromPC(cloud, cloud_filtered);
 
   sensor_msgs::PointCloud2 output_pc;
   pcl::toROSMsg(cloud_filtered, output_pc);
@@ -28,6 +31,56 @@ void RealSensePcl::filterColorFromPC(const pcl::PointCloud<pcl::PointXYZHSV>& in
       output_pc.points.push_back(input_pc.points[i]);
     }
   }
+}
+
+void RealSensePcl::filterFloorFromPC(const pcl::PointCloud<pcl::PointXYZRGB>& input_pc,
+                                     pcl::PointCloud<pcl::PointXYZRGB>& output_pc)
+{
+  float in_floor_max_angle = 0.1;
+  float in_max_height = 0.2;
+
+  pcl::PointCloud<pcl::PointXYZ> in_pc;
+  pcl::PointCloud<pcl::PointXYZ> out_pc;
+  pcl::copyPointCloud(input_pc, in_pc);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr input = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >(in_pc);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr output (new pcl::PointCloud<pcl::PointXYZ>);
+
+  // boost::shared_ptr<const pcl::PointCloud<pcl::PointXYZ> > output = boost::make_shared(output_pc);
+
+
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+
+  seg.setOptimizeCoefficients(true);
+  seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setMaxIterations(1000);
+  seg.setAxis(Eigen::Vector3f(0, 1, 0));
+  seg.setEpsAngle(in_floor_max_angle);
+
+
+
+  seg.setDistanceThreshold(in_max_height);  // floor distance
+  seg.setOptimizeCoefficients(true);
+  seg.setInputCloud(input);
+  seg.segment(*inliers, *coefficients);
+  std::cout << "ULALAL" << std::endl;
+  if (inliers->indices.size() == 0)
+  {
+    std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+  }
+
+  // REMOVE THE FLOOR FROM THE CLOUD
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  extract.setInputCloud(input);
+  extract.setIndices(inliers);
+  extract.setNegative(true);  // true removes the indices, false leaves only the indices
+  extract.filter(*output);
+
+  pcl::copyPointCloud(*output, output_pc);
+
 }
 
 void RealSensePcl::convertPCXYZRGBtoXYZHSV(const pcl::PointCloud<pcl::PointXYZRGB>& input_pc,
