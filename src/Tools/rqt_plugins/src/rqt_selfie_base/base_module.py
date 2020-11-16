12665185a25a44edf1fe7995c9f2bf6b52d3c14d
pktiuk <kotiuk@zohomail.eu>
2020-11-16 23:41:45 +0100

    Add basic functionality

--- src/Tools/rqt_plugins/src/rqt_selfie_base/base_module.py
+++ src/Tools/rqt_plugins/src/rqt_selfie_base/base_module.py
@@ -5,9 +5,15 @@ import rospkg
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
+from std_srvs.srv import Empty
+
+from custom_msgs.msg import Buttons
 
 
 class MyPlugin(Plugin):
+    BUTTON_TOPIC_NAME = "selfie_out/buttons"
+    CHANGE_RC_SERVICE_NAME = "switch_state"
+    RES_VISION_SERVICE_NAME = "/reset_vison"
 
     def __init__(self, context):
         super(MyPlugin, self).__init__(context)
@@ -45,24 +51,45 @@ class MyPlugin(Plugin):
         self._widget.button_res_odometry.pressed.connect(self.restart_odometry)
         self._widget.button_res_vision.pressed.connect(self.restart_vision)
 
+        # init publishers and subscribers
+        self.pub_button = rospy.Publisher(self.BUTTON_TOPIC_NAME, Buttons)
+        self.srv_change_rc = None
+        self.srv_res_lane = None
+        self.srv_res_odometry = None
+        self.srv_res_vision = rospy.ServiceProxy(
+            self.RES_VISION_SERVICE_NAME, Empty)
+
     def press_button1(self):
-        pass
+        rospy.logdebug("Pressed button1 button")
+        msg = Buttons
+        msg.is_pressed_first = True
+        msg.is_pressed_second = False
+        self.pub_button.publish(msg)
 
     def press_button2(self):
-        pass
+        rospy.logdebug("Pressed 'button2' button")
+        msg = Buttons
+        msg.is_pressed_first = False
+        msg.is_pressed_second = True
+        self.pub_button.publish(msg)
 
     def change_rc_mode(self):
-        pass
+        rospy.logdebug("Pressed change RC button")
 
     def restart_lane_control(self):
-        pass
+        rospy.logdebug("Pressed restart lane_control button")
 
     def restart_odometry(self):
-        pass
+        rospy.logdebug("Pressed restart odometry button")
 
     def restart_vision(self):
-        pass
+        rospy.logdebug("Pressed restart vision button")
+        service_list = rosservice.get_service_list()
+        if self.RES_VISION_SERVICE_NAME not in service_list:
+            rospy.WARN(self.RES_VISION_SERVICE_NAME +
+                       " service server is not active")
+
+        response = self.srv_res_vision
 
     def shutdown_plugin(self):
-        # TODO unregister all publishers here
-        pass
+        self.pub_button.unregister()
