deca37a42df9d1b5f61ebbfe64744026f4a8a26a
pktiuk <kotiuk@zohomail.eu>
2021-01-14 19:30:45 +0100

    Remove new code

--- src/Shared/custom_msgs/README.md
+++
@@ -1,3 +0,0 @@
-# Custom Messages and actions Wiki
-
-Here we have actions and messages which are used by nodes from different packages.
--- src/Tools/setup/camera_basler/basler.sh
+++
@@ -1,9 +0,0 @@
-wget "https://www.baslerweb.com/fp-1551786516/media/downloads/software/pylon_software/pylon-5.2.0.13457-x86_64.tar.gz"
-tar xvzf pylon-5.2.0.13457-x86_64.tar.gz
-cd pylon-5.2.0.13457-x86_64
-sudo tar -C /opt -xzf pylonSDK*.tar.gz
-cd ..
-rm -r pylon-5.2.0.13457-x86_64
-rm pylon-5.2.0.13457-x86_64.tar.gz
-sudo sh -c 'echo "yaml https://raw.githubusercontent.com/magazino/pylon_camera/indigo-devel/rosdep/pylon_sdk.yaml " > /etc/ros/rosdep/sources.list.d/15-plyon_camera.list'
-rosdep update
