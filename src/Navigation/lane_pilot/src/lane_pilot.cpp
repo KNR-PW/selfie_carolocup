f7980fce664b30bb8f0d84e9449a3a80e75bdb79
Maciejeg <maciek.majek2@gmail.com>
2022-02-16 19:48:03 +0100

    Implement speed matching for obstacle tracking

--- src/Navigation/lane_pilot/src/lane_pilot.cpp
+++ src/Navigation/lane_pilot/src/lane_pilot.cpp
@@ -39,6 +39,7 @@ RoadObstacleDetector::RoadObstacleDetector(const ros::NodeHandle& nh, const ros:
   pnh_.param<int>("num_proof_to_slowdown", num_proof_to_slowdown_, 2);
   pnh_.param<int>("num_corners_to_detect", num_corners_to_detect_, 3);
   pnh_.param<float>("lane_change_distance", lane_change_distance_, 0.9);
+  pnh_.param<float>("target_distance_to_obstacle", target_distance_to_obstacle_, 0.2);
 
   num_proof_to_return_ = num_proof_to_slowdown_;  // Maybe change to param later
   dr_server_.setCallback(dr_server_CB_);
@@ -117,10 +118,37 @@ void RoadObstacleDetector::obstacleCallback(const custom_msgs::Box2DArray& msg)
     if (!filtered_boxes_.empty())
     {
       ++proof_slowdown_;
-      if (can_overtake_ &&
-          (state_ == EnumLaneControl::ON_RIGHT || state_ == EnumLaneControl::RETURN_RIGHT) &&
-          (nearest_box_in_front_of_car_->bl.x <= max_distance_to_obstacle_ ||
-           nearest_box_in_front_of_car_->br.x <= max_distance_to_obstacle_))
+      if (!can_overtake_)
+      {
+        float distance = std::min(nearest_box_in_front_of_car_->bl.x,
+                                  nearest_box_in_front_of_car_->br.x);
+        if (distance > 2*target_distance_to_obstacle_)
+        {
+          speed_message_.data = max_speed_;
+          offset_value_.data = right_lane_offset_;
+          speed_pub_.publish(speed_message_);
+          offset_pub_.publish(offset_value_);
+          return;
+        }
+
+        float error = target_distance_to_obstacle_ - distance;
+        if (error < 0)
+        {
+          speed_message_.data = std::min(std::abs(max_speed_ * error), max_speed_);
+        }
+        else
+        {
+          speed_message_.data = 0;
+        }
+        offset_value_.data = right_lane_offset_;
+
+        speed_pub_.publish(speed_message_);
+        offset_pub_.publish(offset_value_);
+        return;
+      }
+      else if ((state_ == EnumLaneControl::ON_RIGHT || state_ == EnumLaneControl::RETURN_RIGHT) &&
+              (nearest_box_in_front_of_car_->bl.x <= max_distance_to_obstacle_ ||
+               nearest_box_in_front_of_car_->br.x <= max_distance_to_obstacle_))
       {
         proof_slowdown_ = 0;
         calculateReturnDistance();
