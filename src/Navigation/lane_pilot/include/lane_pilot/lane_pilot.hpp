e47163aac7db4e1b2ac2406a2d3a8932bf147d9d
Maciejeg <maciek.majek2@gmail.com>
2022-04-03 15:05:15 +0200

    Yet another code formatting

--- src/Navigation/lane_pilot/include/lane_pilot/lane_pilot.hpp
+++ src/Navigation/lane_pilot/include/lane_pilot/lane_pilot.hpp
@@ -18,8 +18,8 @@
 #include <dynamic_reconfigure/client.h>
 #include <dynamic_reconfigure/server.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h>
 #include <std_srvs/Empty.h>
-
 #include <lane_pilot/LaneControllerConfig.h>
 #include <common/marker_visualization.h>
 #include <common/state_publisher.h>
@@ -45,11 +45,8 @@ private:
   ros::Subscriber obstacles_sub_;
   ros::Subscriber road_lines_sub_;
   ros::Subscriber motion_sub_;
-<<<<<<< HEAD
-=======
   ros::Subscriber overtaking_sub_;
   ros::Subscriber speed_limit_sub_;
->>>>>>> e39564f... Implement speed limiter
   ros::Publisher speed_pub_;
   ros::Publisher visualizer_;
   ros::Publisher offset_pub_;
@@ -97,12 +94,9 @@ private:
                                                // "lane_change_distance_" meters
   float distance_when_started_changing_lane_;  // saved when we begin changing lane
 
-<<<<<<< HEAD
-=======
   float target_distance_to_obstacle_;
   bool can_overtake_;
   bool speed_limit_;
->>>>>>> e39564f... Implement speed limiter
   int proof_slowdown_;
   int num_proof_to_slowdown_;
   int proof_return_;
@@ -135,11 +129,8 @@ private:
                                                           // lane
   void obstacleCallback(const custom_msgs::Box2DArray&);
   void motionCallback(const custom_msgs::Motion&);
-<<<<<<< HEAD
-=======
   void overtakingCallback(const std_msgs::Bool&);
   void speedCallback(const std_msgs::Bool&);
->>>>>>> e39564f... Implement speed limiter
   void calculateReturnDistance();
 
   bool switchToActive(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
